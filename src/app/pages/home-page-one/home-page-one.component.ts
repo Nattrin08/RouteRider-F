7d9a541d9f1637abf3d213b44e8924ea6beea51e
Nattrin <natalia.rin08@gmail.com>
2023-06-02 09:03:18 -0500

    fixed links

--- src/app/pages/home-page-one/home-page-one.component.ts
+++ src/app/pages/home-page-one/home-page-one.component.ts
@@ -11,6 +11,10 @@ export class HomePageOneComponent {
   heightDevice: number;
   videoPath: string ="assets/vid/video1.mp4";
   videoPath2: string ="assets/vid/video3.mp4";
+
+  get isMobileView(): boolean {
+    return window.innerWidth <= 992;
+  }
   //Crea o asigna los valores que se estan asignando en la clase
   constructor(private router: Router) {
     this.heightDevice = window.innerHeight;
@@ -20,21 +24,5 @@ export class HomePageOneComponent {
   onResize(event: any) {
     //event.target.innerHeight = define el alto de la ventana en la que se esta presentando
     this.heightDevice = event.target.innerHeight;
-  }
-  //constructor para la funcion del boton hacia rutas
-  routesRedirections() {
-    this.router.navigate(['/routes']);
-  }
-  //constructor para la funcion del boton hacia inicio de sesion
-  logInRedirections() {
-    this.router.navigate(['/login']);
-  }
-  //constructor para la funcion del boton hacia registro
-  signUpRedirections() {
-    this.router.navigate(['/register']);
-  }
-  //constructor para la funcion del boton hacia contacto
-  contactRedirections() {
-    this.router.navigate(['/contact']);
   }
 }
